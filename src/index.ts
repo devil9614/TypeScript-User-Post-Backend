fafeed43516e646daf241b48a41c18be6ee07008
Satwik Ghosh <satwikghosh2000@gmail.com>
2021-09-21 15:33:14 +0530

    Added CRUD operations for basic entity

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core"
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from 'express'
+import {ApolloServer} from 'apollo-server-express';
+import {buildSchema} from 'type-graphql';
+import { HelloResolver } from "./resolvers/hello";
+import { Server } from "http";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
@@ -12,8 +19,25 @@ const main = async () => {
     // await orm.em.persistAndFlush(post);
 
     //Finding the post from the DB
-    const posts = await orm.em.find(Post, {});
-    console.log(posts);
+    // const posts = await orm.em.find(Post, {});
+    // console.log(posts);
+
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostResolver],
+            validate: false
+        }),
+        context: () => ({ em: orm.em })
+    });
+
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(4000, () => {
+        console.log('Server started on localhost:4000');
+    })
 };
 
 main().catch((err) => {
