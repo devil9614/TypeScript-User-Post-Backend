4a44b4ff8ad37ce84d0749f3a19e4868d33f9dd0
Satwik Ghosh <satwikghosh2000@gmail.com>
2021-09-21 13:08:37 +0530

    Added Migration CLI

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,14 @@
 import { MikroORM } from "@mikro-orm/core"
-import { errorMonitor } from "events";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
 
 const main = async () => {
-    const orm = await MikroORM.init({
-        entities: [Post],
-        dbName: 'typeserver',
-        user: 'postgres',
-        password: 'postgresql',
-        type: 'postgresql',
-        debug: !__prod__
-    });
-
+    const orm = await MikroORM.init(microConfig);
+    await orm.getMigrator().up();
     const post = orm.em.create(Post, {title: 'my first post'});
     await orm.em.persistAndFlush(post);
-}
+};
 
 main().catch((err) => {
     console.error(err);
